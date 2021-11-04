c3060b6aa23d5c1668e3ff337bcd68610f623dde
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-03 22:38:03 -0300

    Add files via upload

---
+++ disciplinas/programacao-imperativa/mumuki/capitulo-01.js
@@ -0,0 +1,177 @@
+// Capítulo 1: Fundamentos de programação
+
+// 1.Primeiros Programas
+// Exercícío 1
+
+function metade(numero) {
+    return numero / 2;
+ }
+ 
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+// Exercícío 13
+
+
+// 2.Prática Primeiros Programas
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+
+
+// 3.Procedimentos
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+// Exercícío 13
+// Exercícío 14
+// Exercícío 15
+// Exercícío 16
+
+
+// 4.Prática Procedimentos
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+
+
+
+// 5.Repetição Simples
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+
+
+// 6. Prática da Repetição Simples
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+
+
+// 7. Expressões
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+
+
+// 8. Alternativa Condicional
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+
+
+// 9. Funções
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+// Exercícío 13
+// Exercícío 14
+
+
+// 10. Prática de Distribuir Livros
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+
+
+// 11. Repetição condicional
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+
+
+// 12. Variáveis
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
---
+++ disciplinas/programacao-imperativa/mumuki/capitulo-02.js
@@ -0,0 +1,83 @@
+// Capítulo 2: Programação imperativa
+
+// 1.Funções e Tipos de Dados
+// Exercícío 1
+
+function metade(numero) {
+    return numero / 2;
+ }
+ 
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+// Exercícío 13
+// Exercícío 14
+// Exercícío 15
+// Exercícío 16
+
+// 2.Prática Funções e Tipos de Dados
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+
+// 3.Variáveis e Procedimentos
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+
+// 4.Lógica booleana
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+
+// 5.Arrays
+// Exercícío 1
+// Exercícío 2
+// Exercícío 3
+// Exercícío 4
+// Exercícío 5
+// Exercícío 6
+// Exercícío 7
+// Exercícío 8
+// Exercícío 9
+// Exercícío 10
+// Exercícío 11
+// Exercícío 12
+// Exercícío 13
+// Exercícío 14
+// Exercícío 15
+// Exercícío 16
+// Exercícío 17
+// Exercícío 18
+// Exercícío 19
