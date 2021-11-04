c3ac930b41562dc11e1c434c05e76f139ccc2f4f
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-04 14:23:09 -0300

    Update capitulo-01.js

--- disciplinas/programacao-imperativa/mumuki/capitulo-01.js
+++ disciplinas/programacao-imperativa/mumuki/capitulo-01.js
@@ -2,26 +2,88 @@
 
 // 1.Primeiros Programas
 // Exercícío 1
+//Next
 
-function metade(numero) {
-    return numero / 2;
- }
- 
 // Exercícío 2
- 
-
+//Next
 
 // Exercícío 3
+program {
+  Mover(Norte)
+}
+
 // Exercícío 4
+program {
+  Mover(Norte)
+  Mover(Norte)
+  Mover(Norte)
+}
+
 // Exercícío 5
+program{
+  Mover(Leste)
+  Mover(Leste)
+  Mover(Sul)
+}
+
 // Exercícío 6
+program {
+  Mover(Norte)
+  Mover(Leste)
+}
+
 // Exercícío 7
+program{
+  Mover(Norte)  
+  Mover(Norte)  
+  Mover(Norte)  
+  Mover(Norte)  
+}
+
 // Exercícío 8
+program {
+  Colocar(Vermelho)    
+}
+
 // Exercícío 9
+program {
+  Colocar(Vermelho)
+  Colocar(Vermelho)
+  Colocar(Azul)
+  Colocar(Verde)
+  Colocar(Vermelho)
+}
+
 // Exercícío 10
+program {
+  Colocar(Vermelho)
+  Colocar(Vermelho)
+  Colocar(Vermelho)
+  Colocar(Vermelho)
+  Colocar(Preto)
+  Colocar(Preto)
+  Colocar(Preto)
+}
+
 // Exercícío 11
+program {
+  Mover(Sul)
+  Retirar(Vermelho)
+}
+
 // Exercícío 12
+program {
+  Retirar(Verde)
+}
+
 // Exercícío 13
+program {
+  Retirar(Vermelho)
+  Retirar(Preto)
+  Retirar(Azul)
+  Retirar(Verde)
+  
+}
 
 
 // 2.Prática Primeiros Programas
