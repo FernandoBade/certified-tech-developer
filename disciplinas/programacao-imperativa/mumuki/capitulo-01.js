e385dff3e63db55ce534f8a2877384ab0142602b
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-04 20:04:26 -0300

    Update capitulo-01.js

--- disciplinas/programacao-imperativa/mumuki/capitulo-01.js
+++ disciplinas/programacao-imperativa/mumuki/capitulo-01.js
@@ -82,20 +82,115 @@ program {
   Retirar(Preto)
   Retirar(Azul)
   Retirar(Verde)
-  
 }
 
 
 // 2.Prática Primeiros Programas
 // Exercícío 1
-// Exercícío 2
-// Exercícío 3
-// Exercícío 4
-// Exercícío 5
-// Exercícío 6
-// Exercícío 7
-// Exercícío 8
+program {
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Preto)
+}
 
+// Exercícío 2
+program {
+  Colocar(Vermelho)
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Colocar(Vermelho)
+ }
+
+// Exercícío 3
+program {
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+ }
+
+// Exercícío 4
+program {
+  Colocar(Azul)
+  Mover(Leste)
+  Colocar(Azul)
+  Mover(Leste)
+  Colocar(Azul)
+  Mover(Norte)
+  Mover(Oeste)
+  Colocar(Azul)
+  Mover(Oeste)
+  Colocar(Azul)
+  Mover(Norte)
+  Colocar(Azul)
+}
+
+// Exercícío 5
+program {
+  Colocar(Verde)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Mover(Norte)
+  Colocar(Vermelho)
+  Mover(Oeste)
+  Colocar(Vermelho)
+  Mover(Oeste)
+  Colocar(Verde)
+}
+
+// Exercícío 6
+program {
+  Mover(Leste)
+  Mover(Norte)
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Mover(Leste)
+  Colocar(Vermelho)
+  Mover(Oeste)
+  Mover(Norte)
+  Colocar(Vermelho)
+}
+
+// Exercícío 7
+program {
+  Retirar(Verde)
+  Mover(Leste)
+  Retirar(Verde)
+  Mover(Leste)
+  Retirar(Verde)
+  Mover(Norte)
+  Retirar(Verde)
+  Mover(Norte)
+  Retirar(Verde)
+  Mover(Oeste)
+  Retirar(Verde)
+  Mover(Oeste)
+  Retirar(Verde)
+  Mover(Sul)
+  Retirar(Verde)
+}
+
+// Exercícío 8
+program {
+  Retirar(Vermelho)
+  Colocar(Verde)
+  Mover(Leste)
+  Retirar(Vermelho)
+  Colocar(Verde)
+  Mover(Norte)
+  Retirar(Vermelho)
+  Colocar(Verde)
+  Mover(Oeste)
+  Retirar(Vermelho)
+  Colocar(Verde)
+}
 
 // 3.Procedimentos
 // Exercícío 1
