0bf7b193c66cfb9ee5f8cf595f0e4d14c8c8d865
FernandoBade <bade.fernando@gmail.com>
2021-11-08 21:25:59 -0300

    Upload via add files

--- disciplinas/programacao-imperativa/aula-08/exercicio-igualdade-if-else.js
+++ disciplinas/programacao-imperativa/aula-08/exercicio-igualdade-if-else.js
@@ -1,8 +1,8 @@
-//Crie uma variável chamada linguagem, que terá uma string atribuída a ela. Em seguida, construa uma condicional if/else para que, se a variável estiver armazenando o valor "javascript", ela deve imprimir no console, o texto: "Estou aprendendo". Contudo, caso a variável esteja armazenando qualquer outra linguagem, imprima "Aprenderei mais tarde".//
+//Crie uma variável chamada linguagem, que terá uma string atribuída a ela. Em seguida, construa uma condicional if/else para que, se a variável estiver armazenando o valor "javascript", ela deve imprimir no console, o texto: "Estou aprendendo". Contudo, caso a variável esteja armazenando qualquer outra linguagem, imprima "Aprenderei mais tarde.//
 
 let linguagem = "Javascript"
 
     if (linguagem == "Javascript"){
     console.log("Estou Aprendendo")
     } else { console.log("Aprenderei mais Tarde")
-        }
+      }
