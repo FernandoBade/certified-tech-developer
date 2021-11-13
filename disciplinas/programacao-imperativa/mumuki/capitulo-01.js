9db54bc3e57734836c8b55d998f8a39165eaa8d9
Fernando Bade <89167737+FernandoBade@users.noreply.github.com>
2021-11-12 22:37:10 -0300

    Update capitulo-01.js

--- disciplinas/programacao-imperativa/mumuki/capitulo-01.js
+++ disciplinas/programacao-imperativa/mumuki/capitulo-01.js
@@ -514,28 +514,186 @@ program{
 
 // 5.Repetição Simples
 // Exercícío 1
-// Exercícío 2
-// Exercícío 3
-// Exercícío 4
-// Exercícío 5
-// Exercícío 6
-// Exercícío 7
-// Exercícío 8
-// Exercícío 9
-// Exercícío 10
-// Exercícío 11
+procedure MoverOeste10() {
+  repeat(10) {
+    Mover(Oeste)
+  }
+}
 
+// Exercícío 2
+procedure MoverOeste10() {
+  repeat(10) {
+    Mover(Oeste)
+  }
+}
+
+// Exercícío 3
+procedure MoverOeste5() {
+  repeat(5) {
+    Mover(Oeste)
+  }
+}
+
+// Exercícío 4
+procedure Colocar3AoNordeste(){
+  Mover(Norte)
+  Mover(Leste)
+  repeat(3) {
+   Colocar(Preto)
+  }
+}
+
+// Exercícío 5
+procedure ColocarAzulLonge(){
+  repeat(4) {
+    Mover(Leste)
+  }
+  Colocar(Azul)
+}
+
+// Exercícío 6
+procedure DesenharLinhaPreta6(){
+  repeat(6){
+    Colocar(Preto)
+    Mover(Leste)
+  }
+}
+
+// Exercícío 7
+procedure LinhaVermelha4() {
+  repeat(4) {
+   Colocar(Vermelho)
+   Mover(Norte)
+  }
+}
+
+// Exercícío 8
+procedure Diagonal4Azul (){
+  repeat(4) {
+   Colocar(Azul)
+   Mover(Norte)
+   Mover(Leste)
+  }
+}
+
+// Exercícío 9
+procedure DiagonalPesada4Azul  (){
+  repeat(4) {
+   repeat(21){Colocar(Azul)}
+   Mover(Norte)
+   Mover(Leste)
+  }
+}
+
+// Exercícío 10
+procedure LinhaPreta4Leste() {
+  repeat(3) {
+    Colocar(Preto)
+    Mover(Leste)
+  } 
+    Colocar(Preto)
+}
+
+// Exercícío 11
+procedure QuadradoPreto4 (){
+  repeat(3){
+     LinhaPreta4Leste()
+     repeat(3) {Mover(Oeste)}
+     Mover(Norte)
+} LinhaPreta4Leste()
+}
 
 // 6. Prática da Repetição Simples
 // Exercícío 1
+procedure Diagonal4AzulVoltando(){
+   Diagonal4Azul()
+   repeat(4){
+     Mover(Sul)
+     Mover(Oeste)
+  }
+}
+
 // Exercícío 2
+procedure BandaDiagonal4(){
+  repeat(3){
+  Diagonal4AzulVoltando()
+  Mover(Norte)
+} repeat(3){Mover(Sul)}
+}
+
 // Exercícío 3
+procedure ColocarN(quantidade, cor){
+  repeat(quantidade){
+    Colocar(cor)
+}
+}
+
 // Exercícío 4
+procedure FimDaDitaduraMilitar (){
+  ColocarN(15, Azul)
+  Mover(Leste)
+  ColocarN(1, Verde)
+  Mover(Leste)
+  ColocarN(1985, Preto)
+}
+
+
 // Exercícío 5
+procedure Data(dia, mes, ano){
+  ColocarN(dia, Azul)
+  Mover(Leste)
+  ColocarN(mes, Verde)
+  Mover(Leste)
+  ColocarN(ano, Preto)
+}
+
 // Exercícío 6
+procedure MoverN(quantidade, direcao){
+  repeat(quantidade){
+    Mover(direcao)
+  }
+}
+
 // Exercícío 7
+procedure DesenharRelogio(raio){
+  MoverN(raio, Norte)
+  ColocarN(12, Vermelho)  
+  MoverN(2*raio, Sul)
+  ColocarN(6, Vermelho)
+  MoverN(raio, Norte)
+  ColocarN(0, Vermelho) 
+  MoverN(raio, Oeste)
+  ColocarN(9, Vermelho)  
+  MoverN(2*raio, Leste)
+  ColocarN(3, Vermelho)
+  MoverN(raio, Oeste)
+}
+
 // Exercícío 8
+procedure LinhaLestePesada(peso, cor, comprimento){
+  repeat(comprimento){
+    ColocarN(peso, cor)
+    MoverN(1,Leste)
+ }
+  repeat(comprimento){
+  Mover(Oeste)
+  }
+}
+
 // Exercícío 9
+procedure Azulejo(){
+  ColocarN(1, Verde)
+  ColocarN(5, Preto)
+  ColocarN(9, Vermelho)
+ }
+
+procedure GuardaDe5(){
+  repeat(4){
+    Azulejo()
+    Mover(Leste)
+  } Azulejo()
+}
+
 // Exercícío 10
 
 
