c79fe31fe167baeb6495494f63d882ce5283068c
FernandoBade <bade.fernando@gmail.com>
2021-11-29 23:01:33 -0300

    Update "aula-16"

--- disciplinas/programacao-imperativa/aula-16/aula-16-mesa.js
+++ disciplinas/programacao-imperativa/aula-16/aula-16-mesa.js
@@ -5,24 +5,23 @@
 // ●	Se o número a ser impresso for um múltiplo de seu PRIMEIRO parâmetro, você deve imprimir a palavra “Fizz” em vez do número correspondente;
 // ●	Se o número a ser impresso for um múltiplo do seu SEGUNDO parâmetro, você deve imprimir a palavra "Buzz" em vez do número correspondente;
 // ●	Se o número for um múltiplo de AMBOS os parâmetros, você deve imprimir a palavra “FizzBuzz”.
-
 function fizzBuzz(n1, n2) {
     for (let i = 0; i <= 100; i++) {
-        
-        if (i % n1 == 0) {
-            console.log("Fizz") 
+
+        if (i % n1 === 0 && i % n2 != 0) {
+            console.log("fizz")
         }
-        else if  (i%n2 == 0) {
-            console.log("Buzz")
+        else if (i % n2 === 0  && i % n1 != 0) {
+            console.log("buzz")
         }
-        else if (i % n1 == 0 && i % n2 == 0) {
-            console.log("FIZZ BUZZ")
+        else if (i % n2 === 0 && i % n1 === 0) {
+            console.log("fizz buzz")
         }
         else {
             console.log(i)
         }
     }
-        
-    }
 
-    fizzBuzz(2,5);
+}
+
+fizzBuzz(2, 5);
