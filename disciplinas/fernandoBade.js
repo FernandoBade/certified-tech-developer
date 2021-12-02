70f6893ec6b8b9c2e6f2a72b519367b799036c84
FernandoBade <bade.fernando@gmail.com>
2021-12-01 21:23:36 -0300

    Update "aula-22"

---
+++ disciplinas/fernandoBade.js
@@ -0,0 +1,9 @@
+const feBade = {
+    idade: 35,
+    estudandoHoje: ["html", "css", "javascript", "mysql"],
+    estudareiAmanha: ["react", "angular", "vue", "electron"],
+    ferramentas: ["figma", "photoshop", "vscode"],
+    interesses: []
+
+}
+console.log(feBade);
