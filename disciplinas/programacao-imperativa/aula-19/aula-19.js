b2fe09f497044c592fed083ba05a1031bd22a4f7
FernandoBade <bade.fernando@gmail.com>
2021-12-07 00:04:16 -0300

    Update "aula-19"

---
+++ disciplinas/programacao-imperativa/aula-19/aula-19.js
@@ -0,0 +1,42 @@
+// Tem-se um conjunto de dados contendo a altura e o sexo (M ou F) de 15 pessoas. Faça um programa que calcule e escreva:
+
+// ---------------------------------------
+// a maior e a menor altura do grupo;
+// ---------------------------------------
+
+const listaPessoas = require('./pessoas')
+
+
+let menorAltura = Math.min.apply(Math, listaPessoas.map(function (x) { return x.altura; }))
+
+
+let maiorAltura = Math.max.apply(Math, listaPessoas.map(function (x) { return x.altura; }))
+
+console.log(`A menor altura do grupo é ${menorAltura}m e a maior é ${maiorAltura}m.`)
+
+// ---------------------------------------
+// a média de altura das mulheres;
+// ---------------------------------------
+
+let mulheres = listaPessoas.filter(mulher => mulher.sexo === 'F');
+
+let mediaAlturaMulheres = (listaMulheres) => {
+    let { length } = listaMulheres;
+    return listaMulheres.reduce((x, y) => {
+        return x + (y.altura / length);
+    }, 0);
+};
+
+console.log(`A média de altura entre as mulheres é de ${mediaAlturaMulheres(mulheres).toFixed(2)}m.`)
+
+// ---------------------------------------
+// o número de homens.
+// ---------------------------------------
+
+let totalHomens = 0
+for (i = 0; i < listaPessoas.length; i++) {
+    if (listaPessoas[i].sexo === "M") {
+        totalHomens++;
+    }
+}
+console.log(`O total de homens é de ${totalHomens}.`)
---
+++ disciplinas/programacao-imperativa/aula-19/pessoas.js
@@ -0,0 +1,63 @@
+const pessoas = [
+    {
+        "sexo": "F",
+        "nome": "Abigael Natte",
+        "altura": "1.61"
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Ramon Connell",
+        "altura": "1.75"
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Jarret Lafuente",
+        "altura": "1.55"
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Ansel Ardley",
+        "altura": "1.67"
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Jacki Shurmer",
+        "altura": "1.75"
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Jobi Mawtus",
+        "altura": "1.88"
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Thomasin Latour",
+        "altura": "1.67"
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Lonnie Verheijden",
+        "altura": "1.63"
+    },
+    {
+        
+        "sexo": "M",
+        "nome": "Alonso Wannan",
+        "altura": "1.92"
+    },
+    {
+        
+        "sexo": "F",
+        "nome": "Bendite Huggett",
+        "altura": "1.80"
+    },
+  ]
+
+  module.exports = pessoas
