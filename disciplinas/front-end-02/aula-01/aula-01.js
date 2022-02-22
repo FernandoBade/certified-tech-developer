fb76c65387fbaa974bb6a9d6a3b8b1aa12b80b6d
FernandoBade <bade.fernando@gmail.com>
2022-02-21 21:13:43 -0300

    Update "aula-01"

---
+++ disciplinas/front-end-02/aula-01/aula-01.js
@@ -0,0 +1,13 @@
+let nome = "Fernando Bade"
+
+var idade = 35
+
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
+
+let numeros = [1, 2, 3, 4, 5]
+let resultado = 0
+for (let i = 0; i < numeros.length; i++) {
+    resultado += numeros[i]
+    console.log (resultado)
+}
+
