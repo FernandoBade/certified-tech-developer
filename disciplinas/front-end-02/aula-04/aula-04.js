0052ecf10c1c3d845b90a6b4dc0ea27fc66dbed6
FernandoBade <bade.fernando@gmail.com>
2022-02-23 19:23:58 -0300

    Update "aula-04"

---
+++ disciplinas/front-end-02/aula-04/aula-04.js
@@ -0,0 +1,11 @@
+let usuario = {
+    nome: "Fernando",
+    idade: 35,
+    ehProfessor: false,
+    endereço: {
+        cidade: "Franca",
+        estado: "São Paulo"
+    }
+}
+
+console.log(usuario);
