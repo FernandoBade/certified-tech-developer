2156e67512005fea90a7771c90b964a006c2744a
FernandoBade <bade.fernando@gmail.com>
2022-03-07 20:42:49 -0300

    "Update aula-05"

---
+++ disciplinas/front-end-02/aula-05/mesa/script.js
@@ -0,0 +1,19 @@
+function darkMode() {
+
+    let bodyDm = document.querySelector('body')
+    bodyDm.classList.toggle('body-dm')
+    
+    let titulo = document.querySelector('h1')
+    titulo.classList.toggle('h1-dm')
+    
+    let itens = document.querySelectorAll('.item')
+    itens.forEach(itens => {
+        itens.classList.toggle('item-dm')
+    });
+    
+    let itensH2 = document.querySelectorAll('.item > h2')
+    itensH2.forEach(itensH2 => {
+        itensH2.classList.toggle('item-dm-h2')
+    });
+
+}
