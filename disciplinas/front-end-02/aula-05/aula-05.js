471e720cfe912e7446ab87e8238f2900374549bd
FernandoBade <bade.fernando@gmail.com>
2022-03-07 15:03:05 -0300

    'Update aula-05"

---
+++ disciplinas/front-end-02/aula-05/aula-05.js
@@ -0,0 +1,40 @@
+// function escreverHTML(titulo, texto){
+//     const body = document.getElementById('body');
+//     const meuTemplate = `
+//     <h1>${titulo}</h1>
+//     <p>${texto}</p>
+//     `;
+//     body.innerHTML += meuTemplate
+// }
+
+// escreverHTML('Olá', 'Isso é um exemplo de template strng em HTML')
+// escreverHTML('É dinâmico', 'Podemos inserir elementos HTML mediante o <b>JavaScript</b>')
+// escreverHTML('Facilita a Programação', 'Evita escrever muito código e reutilizar o que escrevemos')
+// escreverHTML('Neste exemplo', 'Utilizamos uma única função para poder escrever 4 vezes em HTML. Já imaginou como seria isso sem esta função?')
+
+
+// let personagem = {
+//     nome: "Michael",
+//     sobrenome: "Scott",
+//     nascimento: {
+//        data: "15/04/1965",
+//        cidade: "Scranton",
+//        estado: "Pensilvânia"
+//     },
+//     imagenUrl : "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/MichaelScott.png/220px-MichaelScott.png",
+//     trabalho: {
+//        cargo: "manager regional",
+//        empresa: "Dunder Mifflin"
+//     }
+//     };
+
+
+// document.querySelector('h1').classList.toggle('darkMode')
+
+
+
+// var nome = prompt("Insira seu nome")
+// var texto = document.createTextNode("Olá " + nome)
+// var p = document.createElement("p")
+// p.appendChild(texto)
+// document.body.appendChild(p)
