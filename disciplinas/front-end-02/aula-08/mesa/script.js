8b0656ac7d82b5ef376595e0206e40b135464f60
FernandoBade <bade.fernando@gmail.com>
2022-03-14 21:13:11 -0300

    Update aula-08

---
+++ disciplinas/front-end-02/aula-08/mesa/script.js
@@ -0,0 +1,11 @@
+function stp (i){
+    i.preventDefault()
+}
+document.getElementById("enviar").addEventListener('onload', stp)
+
+
+// fernando bade
+// geyson kaio
+// issao takeuchi
+// rodrigo lisboa
+// vinicius almeida
