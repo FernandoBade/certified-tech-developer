fbc26b721c2bb7d3302fe0e3b1455ce5d13bc394
FernandoBade <bade.fernando@gmail.com>
2022-03-15 01:23:04 -0300

    Upload aula-08

--- disciplinas/front-end-02/aula-08/mesa/script.js
+++ disciplinas/front-end-02/aula-08/mesa/script.js
@@ -1,11 +1,39 @@
-function stp (i){
-    i.preventDefault()
+//evitar que o submit recarregue a página
+
+document.getElementById("enviar").addEventListener("click", (evento) => { evento.preventDefault() });
+
+//alerta ao terminar de carregar a página, com delay de 2 segundos
+
+// setTimeout(()=>{window.onload = alert("Página carregada com sucesso")}, 2000);
+
+//texto do botão muda ao passar o mouse
+
+document.getElementById('btn').addEventListener('mouseover', passaOMouse)
+
+function passaOMouse() {
+    document.getElementById('btn').style.backgroundColor = "crimson"
+    document.getElementById('btn').style.fontSize = "20px"
+    document.getElementById('btn').style.color = "white"
+    document.getElementById('btn').innerText = "TIRA O MOUSE DE CIMA DE MIM"
 }
-document.getElementById("enviar").addEventListener('onload', stp)
 
+//texto do botão ao retirar o mouse
 
-// fernando bade
-// geyson kaio
-// issao takeuchi
-// rodrigo lisboa
-// vinicius almeida
+document.getElementById('btn').addEventListener('mouseout', tiraOMouse)
+
+function tiraOMouse() {
+    document.getElementById('btn').style.backgroundColor = "purple"
+    document.getElementById('btn').style.fontSize = "20px"
+    document.getElementById('btn').innerText = "Ufá...tá tudo bem agora."
+}
+
+//insert do conteúdo do input no textarea
+
+function contador() {
+    let contagem = document.getElementById('nome').value.length + 1
+    if (contagem === 1) {
+        document.getElementById('texto').innerHTML = `Sua frase tem ${contagem} caractere.`
+    } else {
+        document.getElementById('texto').innerHTML = `Sua frase tem ${contagem} caracteres.`
+    }
+}
