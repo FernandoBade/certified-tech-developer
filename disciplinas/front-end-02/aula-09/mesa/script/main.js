a78db20be00974d40f23b08455946106642bd9ad
FernandoBade <bade.fernando@gmail.com>
2022-03-17 20:59:45 -0300

    Update "aula-09"

--- disciplinas/front-end-02/aula-09/mesa/script/main.js
+++ disciplinas/front-end-02/aula-09/mesa/script/main.js
@@ -1,3 +1,40 @@
+// program for a simple calculator
+
+// take the operator input
+// const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// // take the operand input
+// const number1 = parseFloat(prompt('Enter first number: '));
+// const number2 = parseFloat(prompt('Enter second number: '));
+
+let result
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
+
+
+
+
+
+
+
+
+
+
 // function input(numero) {
 //     let numeros = document.getElementById("resultado").innerHTML;
 //     parseInt(document.getElementById("resultado").innerHTML = numeros + numero);
@@ -15,20 +52,45 @@
 // }
 
 
-var v1 = document.getElementById('n1');
-var v2 = document.getElementById('n2');
+// var v1 = document.getElementById('n1');
+// var v2 = document.getElementById('n2');
+// var operacao = document.getElementById('operacao').innerHTML;
 
-document.getElementById('calcular').addEventListener('click', (event) => {
-    event.preventDefault()
-    calcular()
-})
+// document.getElementById('teste').innerHTML = operacaoEscolhida
+// // var valor = operacao.options[operacao.selectedIndex].value
+// var operacaoEscolhida = operacao.innerText;
 
-// console.log(n1)
-function calcular(){
+// console.log(operacaoEscolhida);
+// console.log(v1.innerText);
 
-    console.log(v1.value);    
-    console.log(v2.value);    
-    teste.innerText = parseInt(v1) + parseInt(v2)
-    // console.log(n1.value + n2.value)
-}
 
+
+// document.getElementById('calcular').addEventListener('click', (event) => {
+//     event.preventDefault()
+//     calcular()
+// })
+
+
+
+
+
+
+
+// const junta = document.getElementById('junta').innerText
+// const tira = document.getElementById('tira').innerText
+// const vezes = document.getElementById('vezes').innerText
+// const reparte = document.getElementById('reparte').innerText
+// const operacao = ""
+
+// console.log(vezes)
+
+// function soma(v1, v2) {
+//     let resultado = parseFloat(v1) + parseFloat(v2)
+//     console.log(resultado)
+// }
+
+// function calcular() {
+//         soma()
+// }
+
+// if somar (valor1, valor2)
---
+++ disciplinas/infraestrutura-01/aula-09/site/script.js
@@ -0,0 +1,39 @@
+//evitar que o submit recarregue a página
+
+document.getElementById("enviar").addEventListener("click", (evento) => { evento.preventDefault() });
+
+//alerta ao terminar de carregar a página, com delay de 2 segundos
+
+setTimeout(()=>{window.onload = alert("Página carregada com sucesso")}, 2000);
+
+//texto do botão muda ao passar o mouse
+
+document.getElementById('btn').addEventListener('mouseover', passaOMouse)
+
+function passaOMouse() {
+    document.getElementById('btn').style.backgroundColor = "crimson"
+    document.getElementById('btn').style.fontSize = "20px"
+    document.getElementById('btn').style.color = "white"
+    document.getElementById('btn').innerText = "( ͡° ͜ʖ ͡°)"
+}
+
+//texto do botão ao retirar o mouse
+
+document.getElementById('btn').addEventListener('mouseout', tiraOMouse)
+
+function tiraOMouse() {
+    document.getElementById('btn').style.backgroundColor = "purple"
+    document.getElementById('btn').style.fontSize = "20px"
+    document.getElementById('btn').innerText = "( ͡° ʖ̯ ͡°)"
+}
+
+//insert do conteúdo do input no textarea
+
+function contador() {
+    let contagem = document.getElementById('nome').value.length + 1
+    if (contagem === 1) {
+        document.getElementById('texto').innerHTML = `Sua frase tem ${contagem} caractere.`
+    } else {
+        document.getElementById('texto').innerHTML = `Sua frase tem ${contagem} caracteres.`
+    }
+}
