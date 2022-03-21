de8096de388b60850c98a339aa646540784402a2
FernandoBade <bade.fernando@gmail.com>
2022-03-21 18:16:47 -0300

    Update exercício aula-11

---
+++ disciplinas/front-end-02/aula-11/exercicio/README.md
@@ -0,0 +1,20 @@
+# Exercício de Prática. ✏️
+
+Neste exercício, partiremos de um formulário de registro implementado anteriormente, onde o usuário vai inserir uma série de dados dos inputs correspondentes.
+
+O exercício consiste em recuperar cada um desses valores usando os métodos que o Javascript nos fornece para isso, a fim de realizar algumas validações e mostrar o resultado esperado, conforme explicado em cada uma das etapas encontradas no arquivo index.js dentro da pasta scripts.js. ✔️
+
+Para fazer isso, você deve examinar e prestar atenção às diferentes tags html e suas diferentes propriedades, para ver como você pode selecionar cada elemento usando javascript. 👀 👀 👀
+
+Para resolver este exercício, você precisará escrever seu código no arquivo index.js localizado dentro da pasta ```scripts```
+
+## Exemplo
+
+Ao preencher o formulário da seguinte forma (modelo em espanhol):
+
+<img src="./assets/form.gif">
+
+O seguinte resultado deve ser obtido (modelo em espanhol):
+<img src="./assets/response.png">
+
+Bora programar! 💻
---
+++ disciplinas/front-end-02/aula-11/exercicio/scripts/index.js
@@ -0,0 +1,51 @@
+const registerBtn = document.querySelector(".register-btn");
+
+registerBtn.addEventListener("click", () => {
+
+
+  /// Escreva aqui o seu código, seguindo estas orientações passo a passo:
+
+  // 1. Obter o valor introduzido no input do e-mail.
+
+  var email = document.getElementById('email-input').value
+  console.log(email);
+
+  // 2. Obter o valor introduzido no input de senha.
+
+  var senha = document.getElementById('password-input').value
+  console.log(senha);
+
+  // 3. Obter o valor do input de rádio.
+
+  var maioridade = document.getElementsByName("legalAge").checked
+  console.log(maioridade);
+
+  // 4. Obter o valor do input com o checkbox.
+
+  var termosCondicoes = document.getElementById('tyc-input').checked
+  console.log(termosCondicoes);
+
+  // 5. Validar se o utilizador for maior de idade. Caso contrário, mostramos
+  // uma mensagem de erro: "Tem de ser maior de idade para se registar no site".
+
+ if (document.getElementById('age_yes').checked == false) {
+    var idadeCheck = false
+    alert("Tem de ser maior de idade para se registar no site")
+  } else {
+    idadeCheck = true
+  }
+  // 6. Validamos se o utilizador aceitou os termos e condições. Caso contrário, exibimos
+  // uma mensagem de erro: "Deve aceitar os termos e condições para se registar no site".
+
+  termosCondicoes === false ? alert('Deve aceitar os termos e condições para se registar no site') : true
+
+  // 7. Por fim, se tudo estiver correto, exibimos um objeto no console com as informações definidas pelo utilizador. 
+  var usuario = {
+    Email: email,
+    Senha: senha,
+    MaiorDeIdade: idadeCheck,
+    AceitouTermos: termosCondicoes
+  }
+  console.log(usuario)
+
+});
