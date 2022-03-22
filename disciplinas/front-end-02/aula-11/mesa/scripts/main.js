026781a8b25f2cf1382fde31ec14c65cb561c80a
FernandoBade <bade.fernando@gmail.com>
2022-03-22 16:38:46 -0300

    Update "mesa-11"

--- disciplinas/front-end-02/aula-11/mesa/scripts/main.js
+++ disciplinas/front-end-02/aula-11/mesa/scripts/main.js
@@ -4,12 +4,19 @@ function sub() {
     var nome = document.getElementById('nome').value
     var sobrenome = document.getElementById('sobrenome').value
     var nomeCompleto = nome.concat(" ",sobrenome)
-    console.log(nome);
-    console.log(nome.trim());
-    console.log(nome.toUpperCase());
-    console.log(nome.toLowerCase());
-    console.log(nomeCompleto);
-    console.log(nomeCompleto.replaceAll('a', '@').replaceAll('e', '3'))
+    // console.log(nome);
+    // console.log(nome.trim());
+    // console.log(nome.toUpperCase());
+    // console.log(nome.toLowerCase());
+    // console.log(nomeCompleto);
+    // console.log(nomeCompleto.replaceAll('a', '@').replaceAll('e', '3'))
+
+    var resultado = document.getElementById('resultado')
+    resultado.innerHTML = `
+    
+        Sem espaços: ${nomeCompleto.trim()} <br>
+        Em maiúscula: ${nomeCompleto.toUpperCase()} <br>
+        Em minúscula: ${nomeCompleto.toLowerCase()} <br>
+        "a" = "@", "e" = "3": ${nomeCompleto.replaceAll('a', '@').replaceAll('e', '3')}
+        `
 }
-
-
