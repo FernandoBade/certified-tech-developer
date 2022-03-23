3772ce0209f6ebcd63f34bb3ba7976387d226f55
FernandoBade <bade.fernando@gmail.com>
2022-03-22 21:33:59 -0300

    update aula-11

--- disciplinas/front-end-02/checkpoint-01/js/main.js
+++ disciplinas/front-end-02/checkpoint-01/js/main.js
@@ -1,46 +1,46 @@
-function adicionarReceita (){
+function adicionarReceita() {
 
-//captura do nome
+    //captura do nome
 
-var nomeCompleto = document.getElementById("nomeCompleto").value.toLowerCase().trim()
-console.log(nomeCompleto);
+    var nomeCompleto = document.getElementById("nomeCompleto").value.toLowerCase().trim()
+    console.log(nomeCompleto);
 
-//primeira letra do nome maiúscula
+    //primeira letra do nome maiúscula
 
-var nomeEmMaiusculo = nomeCompleto.split(" ");
+    var nomeEmMaiusculo = nomeCompleto.split(" ");
 
-for (var i = 0; i < nomeEmMaiusculo.length; i++) {
-    nomeEmMaiusculo[i] = nomeEmMaiusculo[i].charAt(0).toUpperCase() + nomeEmMaiusculo[i].slice(1);
-}
+    for (var i = 0; i < nomeEmMaiusculo.length; i++) {
+        nomeEmMaiusculo[i] = nomeEmMaiusculo[i].charAt(0).toUpperCase() + nomeEmMaiusculo[i].slice(1);
+    }
 
-var nomeCorrigido = nomeEmMaiusculo.join(" ");
-console.log(nomeCorrigido);
+    var nomeCorrigido = nomeEmMaiusculo.join(" ");
+    console.log(nomeCorrigido);
 
-//captura do título da receita
+    //captura do título da receita
 
-var tituloReceita = document.getElementById("tituloReceita").value.toLowerCase().trim()
-console.log(tituloReceita);
+    var tituloReceita = document.getElementById("tituloReceita").value.toLowerCase().trim()
+    console.log(tituloReceita);
 
-//ajuste do título com primeira em maiúscula
+    //ajuste do título com primeira em maiúscula
 
-var tituloEmMaiusculo = tituloReceita.split(" ");
+    var tituloEmMaiusculo = tituloReceita.split(" ");
 
-for (var i = 0; i < tituloEmMaiusculo.length; i++) {
-    tituloEmMaiusculo[i] = tituloEmMaiusculo[i].charAt(0).toUpperCase() + tituloEmMaiusculo[i].slice(1);
-}
+    for (var i = 0; i < tituloEmMaiusculo.length; i++) {
+        tituloEmMaiusculo[i] = tituloEmMaiusculo[i].charAt(0).toUpperCase() + tituloEmMaiusculo[i].slice(1);
+    }
 
-var tituloCorrigido = tituloEmMaiusculo.join(" ");
-console.log(tituloCorrigido);
+    var tituloCorrigido = tituloEmMaiusculo.join(" ");
+    console.log(tituloCorrigido);
 
-//captura da URL da imagem
+    //captura da URL da imagem
 
-var imgReceita = document.getElementById("imgReceita").value
-console.log(imgReceita);
+    var imgReceita = document.getElementById("imgReceita").value
+    console.log(imgReceita);
 
-//captura da descrição da receita
+    //captura da descrição da receita
 
-var descricaoReceita = document.getElementById("descricaoReceita").value
-console.log(descricaoReceita);
+    var descricaoReceita = document.getElementById("descricaoReceita").value
+    console.log(descricaoReceita);
 
 
 
