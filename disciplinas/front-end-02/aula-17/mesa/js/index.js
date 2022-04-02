90e9d3211e634514d6a14db25287cedb5fb58c96
FernandoBade <bade.fernando@gmail.com>
2022-04-02 00:59:25 -0300

    Update "mesa-17"

--- disciplinas/front-end-02/aula-17/mesa/js/index.js
+++ disciplinas/front-end-02/aula-17/mesa/js/index.js
@@ -4,52 +4,54 @@
 // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
 
 
-let criarNovoUsuarop = document.getElementById('recarregar');
-
-
 function chamarApi() {
-
+    
     fetch('https://randomuser.me/api/')
-        .then(function (resultado) {
-            if (resultado.status == 200) {
-                return resultado.json();
-            }
-        }).then((user) => {
-            const users = user.results
-            users.forEach(user => renderizarDadosUsuario(user));
+    .then(resposta => {
+        return resposta.json()
         })
-        .catch((err) => {
-            console.log(err);
+        .then(user => {
+            renderizarDadosUsuario(user)
         });
-}
+    }
+    
+    chamarApi()
+    let img = document.querySelector('#foto')
+    let nome = document.querySelector('#nome')
+    let email = document.querySelector('#email')
+    let registro = document.querySelector('#registro')
 
-
+    let criarNovoUsuario = document.querySelector('#result');
 
 function renderizarDadosUsuario(users) {
-    const card = document.querySelector(".tarjeta")
+
+    img.src = `${users.results[0].picture.large}`
+    nome.innerText = `${users.results[0].name.first} ${users.results[0].name.last} `
+    email.innerHTML = `<b>Email: </b>${users.results[0].email}`
+    registro.innerHTML = `<b>ID: </b>${users.results[0].id.value}`
+
+    // const card = document.querySelector(".tarjeta")
 
 
-    const foto = document.createElement("img")
-    foto.src = users.picture.large;
+    // const foto = document.createElement("img")
+    // foto.src = users.results[0].picture.large;
 
 
-    const nome = document.createElement("h1")
-    nome.innerText = users.name.first + ' ' + users.name.last;
+    // const nome = document.createElement("h1")
+    // nome.innerText = users.results[0].name.first + ' ' + users.results[0].name.last;
 
-    const email = document.createElement("h2")
-    email.innerText = `E-mail: ${users.email}`
+    // const email = document.createElement("h2")
+    // email.innerText = `E-mail: ${users.results[0].email}`
 
 
-    const registro = document.createElement("h3")
-    registro.innerText = `ID: ${users.id.value}`
+    // const registro = document.createElement("h3")
+    // registro.innerText = `ID: ${users.results[0].id.value}`
 
-    card.appendChild(foto)
-    card.appendChild(nome)
-    card.appendChild(email)
-    card.appendChild(registro)
-    console.log(card);
+    // card.appendChild(foto)
+    // card.appendChild(nome)
+    // card.appendChild(email)
+    // card.appendChild(registro)
 }
-
 
 /* --------------------------- Tarefa 2 (extra) --------------------------- */
 // Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
@@ -57,14 +59,8 @@ function renderizarDadosUsuario(users) {
 // Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
 
 
-window.onload = chamarApi()
+criarNovoUsuario.addEventListener('click', () => {
+    chamarApi()
+})
 
-
-// botaoRecarregar.addEventListener('click', function (evento) {
-//     renderizarDadosUsuario()
-// })
-
-function atualizaPagina() {
-    window.location.reload();
-}
 
