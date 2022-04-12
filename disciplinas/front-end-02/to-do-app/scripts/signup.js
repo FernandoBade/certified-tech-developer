3e0bddf340a2666ee35cbc4b816f15008bedef7d
FernandoBade <bade.fernando@gmail.com>
2022-04-12 19:05:04 -0300

    Update validações

--- disciplinas/front-end-02/to-do-app/scripts/signup.js
+++ disciplinas/front-end-02/to-do-app/scripts/signup.js
@@ -6,7 +6,7 @@ const emailCadastro = document.getElementById('inputEmailSignup');
 const senhaCadastro = document.getElementById('inputPasswordSignup');
 const senhaConfCadastro = document.getElementById('inputPasswordConfirmSignup');
 const btnCadastro = document.getElementById('btnSignup');
-const endpointBase = "https://ctd-todo-api.herokuapp.com/v1/";
+const endpointCadastro = "https://ctd-todo-api.herokuapp.com/v1/users";
 
 //Objeto de criação de usuário
 
@@ -35,95 +35,76 @@ let emailCadastroNormalizado;
 let senhaCadastroNormalizado;
 let senhaConfCadastroNormalizado;
 
-//Validação dos campos
-
-let nomeValido = false;
-let sobrenomeValido = false;
-let emailValido = false;
-let senhaValida = false;
-let senhaConfValida = false;
-let senhasConferem = false;
-
 //Verifica se os campos estão vazios
 
-nomeCadastro.addEventListener('input', () => {
-    nomeCadastro.value != "" ? nomeValido = true : nomeValido = false  
+let nomeValido = nomeCadastro.addEventListener('blur', () => {
+    nomeCadastro.value != "" ? nomeValido = true : nomeValido = false
 });
 
-sobrenomeCadastro.addEventListener('input', () => {
+let sobrenomeValido = sobrenomeCadastro.addEventListener('blur', () => {
     sobrenomeCadastro.value != "" ? sobrenomeValido = true : sobrenomeValido = false
 });
 
-emailCadastro.addEventListener('input', () => {
+let emailValido = emailCadastro.addEventListener('blur', () => {
     emailCadastro.value != "" ? emailValido = true : emailValido = false
 });
 
-senhaCadastro.addEventListener('input', () => {
+let senhaValida = senhaCadastro.addEventListener('blur', () => {
     senhaCadastro.value != "" ? senhaValida = true : senhaValida = false
 });
 
+let senhaConfValida = senhaCadastro.addEventListener('blur', () => {
+    senhaCadastro.value != "" ? senhaConfValida = true : senhaValida = false
+});
 
+let senhasConferem = false
+function confereSenhas() {
+    senhaCadastro.value === senhaConfCadastro.value ? senhasConferem = true : alert("Senhas não conferem")
+};
 
-//Popula o objeto de criação de usuário
 
 btnCadastro.addEventListener('click', (e) => {
-    if () {
+    confereSenhas()
+    if (nomeValido && sobrenomeValido && emailValido && senhaValida && senhaConfValida && senhasConferem) {
         e.preventDefault()
         
         //Normalização dos campos
-
+        
         nomeCadastroNormalizado = nomeCadastro.value.trim().toLowerCase();
         sobrenomeCadastroNormalizado = sobrenomeCadastro.value.trim().toLowerCase();
         emailCadastroNormalizado = emailCadastro.value.trim().toLowerCase();
         senhaCadastroNormalizado = senhaCadastro.value.trim();
         senhaConfCadastroNormalizado = senhaConfCadastro.value.trim();
-
+        
         //Insersão dos valores no objeto de criação de usuário
-
+        
         novoUsuario.firstName = nomeCadastroNormalizado;
         novoUsuario.lastName = sobrenomeCadastroNormalizado;
         novoUsuario.email = emailCadastroNormalizado;
         novoUsuario.password = senhaCadastroNormalizado;
-
+        
         console.log(novoUsuario);
-
+        
         // Envio para API
 
-        fetch(endpointBase + "users", configApiCadastro).then(
-            resultado => {
-                return resultado.json();
-            }).then(
+        fetch(endpointCadastro, configApiCadastro)
+            .then(
+                resultado => {
+                    return resultado.json();
+                })
+            .then(
                 resultado => {
                     console.log(resultado);
+                }).
+            catch(
+                erro => {
+                    if (erro = 400) {
+                        alert("Usuário já cadastrado")
+                    };
+                });
 
-                }).catch(
-                    erro => {
-                        console.log(erro);
-                        if (erro = 400) {
-                            alert("Usuário já cadastrado")
-                        }
-                    });
+    } else {
+        alert("Preencha todos os campos")
     }
-    // validaTodosCampos();
+
 });
-
-
-// function validaTodosCampos() {
-//     if (nomeValido && sobrenomeValido && emailValido && senhaValida && senhaConfValida) {
-//         console.log(nomeValido);
-//         console.log(sobrenomeValido);
-//         console.log(emailValido);
-//         console.log(senhaValida);
-//         console.log(senhaConfValida);
-//         console.log(senhasConferem);
-
-//         btnCadastro.innerText = "Cadastrar";
-//     } else {
-//         btnCadastro.innerText = "Preencha todos os campos";
-//         btnCadastro.classList.add("btnDisabled");
-//         btnCadastro.style.background = "var(--cinza-escuro)";
-//         btnCadastro.style.color = "var(--cinza-medio)";
-//         btnCadastro.style.cursor = "default";
-//     }
-// }
-
