445dad18037f309a5f85781cf042f2c12bbeb9ae
FernandoBade <bade.fernando@gmail.com>
2022-04-12 18:30:13 -0300

    Update signup

--- disciplinas/front-end-02/to-do-app/scripts/signup.js
+++ disciplinas/front-end-02/to-do-app/scripts/signup.js
@@ -6,11 +6,32 @@ const emailCadastro = document.getElementById('inputEmailSignup');
 const senhaCadastro = document.getElementById('inputPasswordSignup');
 const senhaConfCadastro = document.getElementById('inputPasswordConfirmSignup');
 const btnCadastro = document.getElementById('btnSignup');
+const endpointBase = "https://ctd-todo-api.herokuapp.com/v1/";
 
-//Normalização dos campos
+//Objeto de criação de usuário
+
+const novoUsuario = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+}
+
+const novoUsuarioJson = JSON.stringify(novoUsuario);
+
+const configApiCadastro = {
+    method: 'POST',
+    body: novoUsuarioJson,
+    headers: {
+        'content-type': 'application/json'
+    }
+}
+
+//Variáveis de normalização dos campos
 
 let nomeCadastroNormalizado;
 let sobrenomeCadastroNormalizado;
+let emailCadastroNormalizado;
 let senhaCadastroNormalizado;
 let senhaConfCadastroNormalizado;
 
@@ -23,14 +44,86 @@ let senhaValida = false;
 let senhaConfValida = false;
 let senhasConferem = false;
 
-//Objeto de criação de usuário
+//Verifica se os campos estão vazios
 
-const novoUsuario = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-}
+nomeCadastro.addEventListener('input', () => {
+    nomeCadastro.value != "" ? nomeValido = true : nomeValido = false  
+});
 
-//Verifica se as senhas Bateman
+sobrenomeCadastro.addEventListener('input', () => {
+    sobrenomeCadastro.value != "" ? sobrenomeValido = true : sobrenomeValido = false
+});
+
+emailCadastro.addEventListener('input', () => {
+    emailCadastro.value != "" ? emailValido = true : emailValido = false
+});
+
+senhaCadastro.addEventListener('input', () => {
+    senhaCadastro.value != "" ? senhaValida = true : senhaValida = false
+});
+
+
+
+//Popula o objeto de criação de usuário
+
+btnCadastro.addEventListener('click', (e) => {
+    if () {
+        e.preventDefault()
+        
+        //Normalização dos campos
+
+        nomeCadastroNormalizado = nomeCadastro.value.trim().toLowerCase();
+        sobrenomeCadastroNormalizado = sobrenomeCadastro.value.trim().toLowerCase();
+        emailCadastroNormalizado = emailCadastro.value.trim().toLowerCase();
+        senhaCadastroNormalizado = senhaCadastro.value.trim();
+        senhaConfCadastroNormalizado = senhaConfCadastro.value.trim();
+
+        //Insersão dos valores no objeto de criação de usuário
+
+        novoUsuario.firstName = nomeCadastroNormalizado;
+        novoUsuario.lastName = sobrenomeCadastroNormalizado;
+        novoUsuario.email = emailCadastroNormalizado;
+        novoUsuario.password = senhaCadastroNormalizado;
+
+        console.log(novoUsuario);
+
+        // Envio para API
+
+        fetch(endpointBase + "users", configApiCadastro).then(
+            resultado => {
+                return resultado.json();
+            }).then(
+                resultado => {
+                    console.log(resultado);
+
+                }).catch(
+                    erro => {
+                        console.log(erro);
+                        if (erro = 400) {
+                            alert("Usuário já cadastrado")
+                        }
+                    });
+    }
+    // validaTodosCampos();
+});
+
+
+// function validaTodosCampos() {
+//     if (nomeValido && sobrenomeValido && emailValido && senhaValida && senhaConfValida) {
+//         console.log(nomeValido);
+//         console.log(sobrenomeValido);
+//         console.log(emailValido);
+//         console.log(senhaValida);
+//         console.log(senhaConfValida);
+//         console.log(senhasConferem);
+
+//         btnCadastro.innerText = "Cadastrar";
+//     } else {
+//         btnCadastro.innerText = "Preencha todos os campos";
+//         btnCadastro.classList.add("btnDisabled");
+//         btnCadastro.style.background = "var(--cinza-escuro)";
+//         btnCadastro.style.color = "var(--cinza-medio)";
+//         btnCadastro.style.cursor = "default";
+//     }
+// }
 
