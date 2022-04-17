9bef029510446d329963e95524afa5c6fe56dd23
FernandoBade <bade.fernando@gmail.com>
2022-04-17 19:06:07 -0300

    Criação da função e estilização da snackbar

--- disciplinas/front-end-02/to-do-app/scripts/signup.js
+++ disciplinas/front-end-02/to-do-app/scripts/signup.js
@@ -1,31 +1,19 @@
 //Captura dos campos de cadastros para
-
 const formCadastro = document.getElementById('formCadastro');
 const nomeCadastro = document.getElementById('inputFirstNameSignup');
 const sobrenomeCadastro = document.getElementById('inputLastNameSignup');
 const emailCadastro = document.getElementById('inputEmailSignup');
 const senhaCadastro = document.getElementById('inputPasswordSignup');
-const btnMostrarSenha1 = document.getElementById('btnMostrarSenha1');
-const btnMostrarSenha2 = document.getElementById('btnMostrarSenha2');
 const senhaConfCadastro = document.getElementById('inputPasswordConfirmSignup');
 const btnCadastro = document.getElementById('btnSignup');
 const endpointCadastro = "https://ctd-todo-api.herokuapp.com/v1/users";
-const token = "";
-//Função de mostrar as senhas
 
-btnMostrarSenha1.addEventListener('click', () => {
-    const mudarType = senhaCadastro.getAttribute('type') === 'password' ? 'text' : 'password';
-    senhaCadastro.setAttribute('type', mudarType);
-    btnMostrarSenha1.classList.toggle('material-icons-outlined-1');
-});
-btnMostrarSenha2.addEventListener('click', () => {
-    const mudarType = senhaConfCadastro.getAttribute('type') === 'password' ? 'text' : 'password';
-    senhaConfCadastro.setAttribute('type', mudarType);
-    btnMostrarSenha2.classList.toggle('material-icons-outlined-1');
-});
+//expressão regular baseada no padrão RFC 5322(o mais recente)
+
+//criação da variável que armazenará o token do usuário
+const tokenAtivo = "";
 
 //Objeto de criação de usuário
-
 const novoUsuario = {
     firstName: "",
     lastName: "",
@@ -33,59 +21,41 @@ const novoUsuario = {
     password: "",
 }
 
-const novoUsuarioJson = JSON.stringify(novoUsuario);
-
-const configApiCadastro = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: novoUsuarioJson
-}
-
-//Variáveis de normalização dos campos
-
-let nomeCadastroNormalizado;
-let sobrenomeCadastroNormalizado;
-let emailCadastroNormalizado;
-let senhaCadastroNormalizado;
-let senhaConfCadastroNormalizado;
 
 
-
+//função de envio do cadastro para a API
 formCadastro.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    nomeCadastroNormalizado = nomeCadastro.value.trim().toLowerCase();
-    sobrenomeCadastroNormalizado = sobrenomeCadastro.value.trim().toLowerCase();
-    emailCadastroNormalizado = emailCadastro.value.trim().toLowerCase();
-    senhaCadastroNormalizado = senhaCadastro.value.trim();
-    senhaConfCadastroNormalizado = senhaConfCadastro.value.trim();
-
+    if(validacaoDeSenhas(senhaCadastro.value, senhaConfCadastro.value)){
+        console.log('Senhas iguais');
     //Insersão dos valores no objeto de criação de usuário
 
-    novoUsuario.firstName = nomeCadastroNormalizado;
-    novoUsuario.lastName = sobrenomeCadastroNormalizado;
-    novoUsuario.email = emailCadastroNormalizado;
-    novoUsuario.password = senhaCadastroNormalizado;
+    novoUsuario.firstName = nomeCadastro.value.trim().toLowerCase();
+    novoUsuario.lastName = sobrenomeCadastro.value.trim().toLowerCase();
+    novoUsuario.email = emailCadastro.value.trim().toLowerCase();
+    novoUsuario.password = senhaCadastro.value.trim().toLowerCase();
 
     console.log(novoUsuario);
+    const novoUsuarioJson = JSON.stringify(novoUsuario);
+
+    const configApiCadastro = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: novoUsuarioJson
+    }
 
     fetch(endpointCadastro, configApiCadastro)
         .then(response => {
-            if (response.status == 201) {
-                return response.json()
-            }
-            throw response;
+            console.log(response);
+            return response.json()
+        }
+        ).catch((error) => {
+            console.log(error);
         })
-        .then(response => {
-            cadastroSucesso(response)
-        })
-        .catch((error) => {
-            if (error.status == 400) {
-                console.log(error)
-            } else {
-                cadastroErro(error)
-            }
-        })
+    } else {
+        criarSnackbar("A senhas não conferem");
+        console.log('Senhas difentes');
+    }
 })
--- disciplinas/front-end-02/to-do-app/scripts/tools.js
+++ disciplinas/front-end-02/to-do-app/scripts/tools.js
@@ -1,8 +1,50 @@
-function trimValue(value){
-    return value.trim();
+//Função de mostrar/visualizar as senhas
+
+const btnMostrarSenha1 = document.getElementById('btnMostrarSenha1');
+const btnMostrarSenha2 = document.getElementById('btnMostrarSenha2');
+btnMostrarSenha1.addEventListener('click', () => {
+    const mudarType = senhaCadastro.getAttribute('type') === 'password' ? 'text' : 'password';
+    senhaCadastro.setAttribute('type', mudarType);
+    btnMostrarSenha1.classList.toggle('material-icons-outlined-1');
+});
+btnMostrarSenha2.addEventListener('click', () => {
+    const mudarType = senhaConfCadastro.getAttribute('type') === 'password' ? 'text' : 'password';
+    senhaConfCadastro.setAttribute('type', mudarType);
+    btnMostrarSenha2.classList.toggle('material-icons-outlined-1');
+});
+
+//validação das senhas
+
+function validacaoDeSenhas(senha1, senha2) {
+    if (senha1 === senha2) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function lowerCaseValue(value){
-    return value.toLowerCase();
+//validação de email via Regex
+
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+function validacaoEmailRegex(email) {
+
+    // check if the email is valid
+    const result = regex.test(email);
+    if (result) {
+        console.log('The email is valid.');
+    }
+    else {
+        let newEmail = alert('E-mail inválido. Confira novamente o campo, por favor:');
+        validacaoEmailRegex(newEmail);
+    }
 }
 
+//criação de snackbars
+
+function criarSnackbar(texto) {
+    const snackbar = document.getElementById('snackbar');
+    snackbar.innerText = texto;
+    snackbar.classList.add('showSnackbar');
+    setTimeout(() => { snackbar.style.visibility = "hidden"}, 5000);
+}
