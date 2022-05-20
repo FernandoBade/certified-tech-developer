b73c4a8efff85c806b3e1b02ae6143fa86a526f7
FernandoBade <bade.fernando@gmail.com>
2022-05-20 18:34:44 -0300

    Update aula-03

--- disciplinas/programacao-orientada-objetos/src/PedraPapelTesouraLagartoSpock.java
+++ disciplinas/programacao-orientada-objetos/src/PedraPapelTesouraLagartoSpock.java
@@ -1,23 +1,8 @@
-//Desafio 2
-//        Faça um programa que permita jogar o jogo "Pedra-Papel-Tesoura". Primeiro, o
-//        aplicativo solicita o nome de cada jogador e, em seguida, qual a opção esse jogador
-//        escolhe para jogar: pedra, papel ou tesoura. O jogo consiste em perguntar qual
-//        opção cada um escolhe e somar pontos ao vencedor (se houver). Defina e use
-//        uma função chamada whichWin ou quemGanha com dois parâmetros com as
-//        jogadas de cada um dos jogadores, que retorna 0 em caso de empate, 1 se o
-//        primeiro vencer, 2 se o segundo vencer. O jogo termina quando o primeiro escolhe
-//        "*" como indicador de fim.
 
-//Mesa:
-//
-//      Fernando Bade
-//      Issao Toribio Takeuchi
-//      Luiz Souza
-//      Matheus Abreu
 
 import java.util.Scanner;
 
-public class PedraPapelTesoura {
+public class PedraPapelTesouraLagartoSpock {
 
     public static void main(String[] args) {
         int ptsJogador1 = 0;
