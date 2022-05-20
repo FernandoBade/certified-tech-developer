dc0c84c5b27b8b718a98be9e38348b614c0aedbc
FernandoBade <bade.fernando@gmail.com>
2022-05-20 15:32:00 -0300

    Update aula-03

--- disciplinas/programacao-orientada-objetos/src/PedraPapelTesoura.java
+++ disciplinas/programacao-orientada-objetos/src/PedraPapelTesoura.java
@@ -45,7 +45,7 @@ public class PedraPapelTesoura {
             switch (opcaoJogador1) {
                 case "1":
                     if (opcaoJogador1.equals(opcaoJogador2)) {
-                        System.out.println("-------------------------------" + System.lineSeparator() + "É um empate! Ninguém pontuou" + System.lineSeparator() + "-------------------------------");
+                        System.out.println("-------------------------------" + System.lineSeparator() + "É um empate! Ninguém pontuou!" + System.lineSeparator() + "-------------------------------");
                     } else if (opcaoJogador2.equals("2")) {
                         System.out.println("-------------------------------" + System.lineSeparator() + nomeJogador2 + " ganhou!" + System.lineSeparator() + "-------------------------------");
                         ptsJogador2++;
@@ -56,7 +56,7 @@ public class PedraPapelTesoura {
                     break;
                 case "2":
                     if (opcaoJogador1.equals(opcaoJogador2)) {
-                        System.out.println("-------------------------------" + System.lineSeparator() + "É um empate! Ninguém pontuou" + System.lineSeparator() + "-------------------------------");
+                        System.out.println("-------------------------------" + System.lineSeparator() + "É um empate! Ninguém pontuou!" + System.lineSeparator() + "-------------------------------");
                     } else if (opcaoJogador2.equals("1")) {
                         System.out.println("-------------------------------" + System.lineSeparator() + nomeJogador1 + " ganhou!" + System.lineSeparator() + "-------------------------------");
                         ptsJogador2++;
@@ -67,7 +67,7 @@ public class PedraPapelTesoura {
                     break;
                 case "3":
                     if (opcaoJogador1.equals(opcaoJogador2)) {
-                        System.out.println("-------------------------------" + System.lineSeparator() + "É um empate! Ninguém pontuou" + System.lineSeparator() + "-------------------------------");
+                        System.out.println("-------------------------------" + System.lineSeparator() + "É um empate! Ninguém pontuou!" + System.lineSeparator() + "-------------------------------");
                     } else if (opcaoJogador2.equals("1")) {
                         System.out.println("-------------------------------" + System.lineSeparator() + nomeJogador2 + " ganhou!" + System.lineSeparator() + "-------------------------------");
                         ptsJogador2++;
@@ -82,24 +82,24 @@ public class PedraPapelTesoura {
                     System.out.println("O jogo acabou!" + System.lineSeparator());
                     System.out.println(ptsJogador1 == 1 ? nomeJogador1 + ": " + ptsJogador1 + " ponto" : nomeJogador1 + ": " + ptsJogador1 + " pontos");
                     System.out.println(ptsJogador2 == 1 ? nomeJogador2 + ": " + ptsJogador2 + " ponto" : nomeJogador2 + ": " + ptsJogador2 + " pontos");
-
-                    System.out.println("-------------------------------");
+                    System.out.println(quemGanha(ptsJogador1, ptsJogador2, nomeJogador1, nomeJogador2));
                     jogando = false;
                     break;
             }
         }
     }
+    public static String quemGanha(int pontosTotaisJogador1, int PontosTotaisJogador2, String nomeJogador1, String nomeJogador2) {
+        if (pontosTotaisJogador1 == PontosTotaisJogador2) {
+
+             System.out.println("O jogo terminou empatado!");
+        } else if (pontosTotaisJogador1 > PontosTotaisJogador2) {
+
+             System.out.println(nomeJogador1 + " venceu!");
+        } else {
+
+             System.out.println(nomeJogador2 + " venceu!");
+        } return "-------------------------------";
+    }
+
 }
-
-//    public static int quemGanha(int jogadasJogador1, int jogadasJogador2) {
-//        if (jogadasJogador1 == jogadasJogador2) {
-//            return 0;
-//        } else if (jogadasJogador1 > jogadasJogador2) {
-//            return 1;
-//        } else {
-//            return 2;
-//        }
-//    }
-
-
 
