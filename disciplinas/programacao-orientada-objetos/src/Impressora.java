1f99b4cd5ec103367f7c51d4b8b5c47634d6d230
FernandoBade <bade.fernando@gmail.com>
2022-05-25 20:43:37 -0300

    Update aula-06

---
+++ disciplinas/programacao-orientada-objetos/src/Impressora.java
@@ -0,0 +1,67 @@
+import java.time.LocalDate;
+
+public class Impressora {
+    private String modelo;
+    private String tipoConexao;
+    private LocalDate dataFabricacao;
+    private int folhasDisponiveis;
+
+    public Impressora(String modelo, String tipoConexao, LocalDate dataFabricacao) {
+        this.modelo = modelo;
+        this.tipoConexao = tipoConexao;
+        this.dataFabricacao = dataFabricacao;
+        this.folhasDisponiveis = 0;
+    }
+
+    public Impressora(String modelo, String tipoConexao, LocalDate dataFabricacao, int folhasDisponiveis) {
+        this.modelo = modelo;
+        this.tipoConexao = tipoConexao;
+        this.dataFabricacao = dataFabricacao;
+        this.folhasDisponiveis = folhasDisponiveis;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipoConexao() {
+        return tipoConexao;
+    }
+
+    public void setTipoConexao(String tipoConexao) {
+        this.tipoConexao = tipoConexao;
+    }
+
+    public LocalDate getDataFabricacao() {
+        return dataFabricacao;
+    }
+
+    public void setDataFabricacao(LocalDate dataFabricacao) {
+        this.dataFabricacao = dataFabricacao;
+    }
+
+    public int getFolhasDisponiveis() {
+        return folhasDisponiveis;
+    }
+
+    public void setFolhasDisponiveis(int folhasDisponiveis) {
+        this.folhasDisponiveis = folhasDisponiveis;
+    }
+
+    private boolean temPapel(boolean temPapel) {
+        return folhasDisponiveis > 0;
+    }
+
+    public void imprimir(String texto) {
+        if (temPapel(true)) {
+            System.out.println("Imprimindo: " + texto);
+            folhasDisponiveis--;
+        } else {
+            System.out.println("Não há papel disponível");
+        }
+    }
+}
