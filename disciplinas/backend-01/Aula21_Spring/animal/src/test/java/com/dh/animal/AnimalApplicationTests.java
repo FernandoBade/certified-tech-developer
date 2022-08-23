dfc2055110dd6ec154f9ea760839dafe2c640542
FernandoBade <bade.fernando@gmail.com>
2022-08-23 20:08:45 -0300

    Update Aula20

---
+++ disciplinas/backend-01/Aula21_Spring/animal/src/test/java/com/dh/animal/AnimalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.animal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
