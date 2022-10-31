32da89021ea306fb041f5751b42b579399a4c715
Fernando Bade <bade.fernando@gmail.com>
2022-10-31 20:38:23 -0300

    Update Aula07

--- disciplinas/front-end-03/Aula07/aula-07/src/App.jsx
+++ disciplinas/front-end-03/Aula07/aula-07/src/App.jsx
@@ -1,31 +1,57 @@
+// import { useState } from "react";
+
+// const App = () => {
+//   const [numero, setNumero] = useState(0);
+//   const [nomes, setNomes] = useState(["Jose", "Maria"]);
+
+
+//   const handleButton = () => {
+//     setNumero(numero + 1);
+//   }
+
+//   const addPessoa = () => {
+//     setNomes([...nomes, "Carla",]);
+//   }
+
+
+//   return (
+//     <div>
+//       <h2>{numero}</h2>
+//       <ul>
+//         {nomes.map((nome) => (
+//           <li>{nome}</li>
+//         ))}
+//       </ul>
+      
+//       <button onClick={handleButton}>Add Número</button>
+//       <button onClick={addPessoa}>Add Nome</button>
+//     </div>
+//   )
+// };
+
+// export default App; 
+
+//************MESA************ */
+
 import { useState } from "react";
 
 const App = () => {
-  const [numero, setNumero] = useState(0);
-  const [nomes, setNomes] = useState(["Jose", "Maria"]);
-
-
-  const handleButton = () => {
-    setNumero(numero + 1);
+  const [itens, setItem] = useState([1,2,3,4,5]);
+  
+  function addItem() {
+    let temp = [...itens]
+    setItem([...itens, itens.length + 1])
   }
-
-  const addPessoa = () => {
-    setNomes([...nomes, "Carla",]);
-  }
-
 
   return (
-    <div>
-      <h2>{numero}</h2>
+    <>    
       <ul>
-        {nomes.map((nome) => (
-          <li>{nome}</li>
+        {itens.map((i) => (
+          <li>{`O item ${i} foi adicionado à lista`}</li>
         ))}
-      </ul>
-      
-      <button onClick={handleButton}>Add Número</button>
-      <button onClick={addPessoa}>Add Nome</button>
-    </div>
+      </ul>          
+      <button onClick={addItem}>Add Item</button>
+    </>
   )
 };
 
