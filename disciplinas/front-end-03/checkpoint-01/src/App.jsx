07498483a6e1d778372fc065561769d72a0c082c
Fernando Bade <bade.fernando@gmail.com>
2022-11-09 20:27:27 -0300

    Update checkpoint-01

---
+++ disciplinas/front-end-03/checkpoint-01/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Cards from 'react-credit-cards'
+import 'react-credit-cards/es/styles-compiled.css'
+function App() {
+  const [numeroDoCartao, setNumeroDoCartao] = useState('')
+
+  const [nomeCompleto, setNomeCompleto] = useState('')
+
+  const [validadeDoCartao, setValidadeDoCartao] = useState('')
+
+  const [codigoDeSeguranca, setNcodigoDeSeguranca] = useState('')
+  
+  const [foco, setFoco] = useState('')
+
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
